6664da281ca537f25bb02c0801612b46ee8b0972
yung <56764505+yungbeatz@users.noreply.github.com>
2022-04-27 01:36:36 +0200

    Create example.js

---
+++ example.js
@@ -0,0 +1,44 @@
+const weather = require('weather-js');
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    name: "weather",
+    description: "Checks a weather forecast",
+    options: [
+        {
+            name: "location",
+            description: "Enter a location",
+            type: "STRING",
+            required: true
+        }
+    ],
+    async execute(interaction){
+    const loc = interaction.options.getString("location");
+
+    weather.find({search: loc, degreeType: "C"}, function(error, result){
+        const errEmbed = new MessageEmbed()
+        .setColor("RED")
+        .setDescription("**⛔ You didn't specify a valid location**")
+
+        if (result === undefined || result.length === 0) return interaction.reply({embeds: [errEmbed]});
+        if (error) return interaction.channel.send(error)
+        const current = result[0].current
+        const location = result[0].location
+        
+      const embed = new MessageEmbed()
+      .setTitle(`Showing the Weather Info for ${current.observationpoint}`)
+      .setDescription(current.skytext)
+      .setThumbnail(current.imageUrl)
+      .setColor("#00ff00")
+      .setTimestamp()
+      .addField("Temperature: ", current.temperature + "°C", true)
+      .addField("Wind Speed: ", current.winddisplay, true)
+      .addField("Humidity: ", `${current.humidity}%`, true)
+      .addField("Timezone: ", `UTC${location.timezone}`, true)
+      .setFooter({text: "Weather Command Made By: @yungbeatz" })
+
+      interaction.reply({embeds: [embed]})
+    })
+
+    }
+}
